e30fe5534cbe66c2f9a02bea43b65e3f9d9d93bc
jitender <jith.choudhary@gmail.com>
2022-07-28 14:11:15 +0530

    first commit

---
+++ arithmatic_operation.py
@@ -0,0 +1,27 @@
+num1 = float(input("enter the first number "))
+num2 = float(input("enter the second number "))
+
+# add two number
+sum = num1 + num2
+
+# Subtract num2 from num1
+sub = num1-num2
+
+# multiply 2 numbers
+multi = num1*num2
+
+# Divide 2 numbers
+div = num1/num2
+
+# modulus of 2 numbers
+mod = num1%num2
+
+# exponent of 2 numbers
+expo = num1**num2
+
+print("the addition  of {0} and {1} is {2} ".format(num1,num2,sum))
+print("the subtraction of {1} from {0} is {2} ".format(num1,num2,sub))
+print("the multiplication  of {0} and {1} is {2} ".format(num1,num2,multi))
+print("the divison  of {0} and {1} is {2} ".format(num1,num2,div))
+print("the modulus of {0} and {1} is {2} ".format(num1,num2,mod))
+print("the exponent  of {0} and {1} is {2} ".format(num1,num2,expo))
